b28f815aec9ebebfa2bc727e98be0cfc4b3239e5
shabrinasy <164464464+shabrinasy@users.noreply.github.com>
2025-07-17 14:36:23 +0700

    Update laporan-arus-kas.blade.php

--- resources/views/filament/pages/laporan-arus-kas.blade.php
+++ resources/views/filament/pages/laporan-arus-kas.blade.php
@@ -32,7 +32,6 @@
                             @foreach ($section['accounts'] as $row)
                                 @if ($row['pemasukan'] !== 0)
                                    <tr>
-                                        <td class="px-3 py-2">{{ $row['keterangan'] }}</td>
                                         <td class="px-3 py-2 text-right">Rp {{ number_format((float) $row['pemasukan'], 0, ',', '.') }}</td>
                                     </tr>
 
@@ -45,7 +44,6 @@
                             @foreach ($section['accounts'] as $row)
                                 @if ($row['pengeluaran'] !== 0)
                                     <tr>
-                                        <td class="px-3 py-2">{{ $row['keterangan'] }}</td>
                                         <td class="px-3 py-2 text-right">(Rp {{ number_format((float) $row['pengeluaran'], 0, ',', '.') }})</td>
                                     </tr>
                                 @endif
